838678562e84e7d029a528ac682e425644f2eb3b
shanzhu <shanzhu@raysengine.com>
2024-09-13 10:09:16 +0800

    disk: Support render edge

--- utility/physics/rigid-body/disk.js
+++ utility/physics/rigid-body/disk.js
@@ -8,15 +8,22 @@ class Disk
         this.velocity = new Vector2();
         this.restitution = 0.0;
         this.acceleration = new Vector2();
+
+        this.fill_color = "#00FFFF";
+        this.edge_color = "#000000";
+        this.edge_width = 0.0;
     }
 
-    static from(body_circle, density, velocity, restitution)
+    static from(body_circle, density, velocity, restitution, fill_color = "#DDDDDD", edge_color = "#000000", edge_width = 0.0)
     {
         let result = new Disk();
         result.body.copy(body_circle);
         result.mass.set_density(density);
         result.velocity.copy(velocity);
         result.restitution = restitution;
+        result.fill_color = fill_color;
+        result.edge_color = edge_color;
+        result.edge_width = edge_width;
         return result;
     }
 
@@ -33,6 +40,11 @@ class Disk
         this.velocity.copy(a.velocity);
         this.restitution = a.restitution;
         this.acceleration.copy(a.velocity);
+
+        this.fill_color = a.fill_color;
+        this.edge_color = a.edge_color;
+        this.edge_width = a.edge_width;
+
         return this;
     }
 
@@ -49,7 +61,10 @@ class Disk
 
     render(canvas)
     {
-        canvas.render_circle(this.body, "#00FFFF");
+        canvas.render_circle(this.body, this.fill_color);
+        if (this.edge_width > 0.0) {
+            canvas.render_wireframe_circle(this.body, this.edge_width, this.edge_color);
+        }
     }
 };
 
